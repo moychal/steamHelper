0ff27aa09356e0895bf5de0539e48909c0a6c8b5
Michael Shintaku <michael.shintaku1@gmail.com>
2014-12-01 15:50:06 -0800

    Reorganized classes, improved flexibility of input
    Organized the $ related code to the HelperWindow rather than the main
    class

--- HelperMain.java
+++ HelperMain.java
@@ -3,139 +3,39 @@
 // Objective: Aids in my virtual items trading with general computing, value storage
 // 			  message prompts, etc.
 
-import javax.swing.*;
 import java.io.*;
-// import java.net.*:
 import java.util.*;
 
 
 public class HelperMain {
-	
-	//keyPrice and keyToRef fluctuate based on personal preference.
-	//"keys" and "Refined" or "Ref" are in-game currencies.
-	public static double keyPrice;
-	public static double keyToRef;
-	
-	//Personal user information.
-	private static String repTft = "";
-	private static String repSource = "";
-	private static String repTrust = "";
-	private static String email = "michael.shintaku1@gmail.com";
-	
-	//To do: implement GUI, make program executable?
-	public static void main (String args[]) throws FileNotFoundException  {
 
-		Scanner input = new Scanner(System.in);
-		/* Executes the program through console
-		File settings = new File("settings.txt");
-		if (settings.exists()) {
-			loadPrices();
-			System.out.println("Key price: " + keyPrice + "\n" + "Refined Price: " + keyToRef);
-			System.out.print("Update prices?: y/n: ");
-			if (input.nextLine().contains("y")) {
-				updatePricesHelper(input);
-				loadPrices();
-			}
-		} else {
-			updatePricesHelper(input);
-			loadPrices();
-		}
-		System.out.print("PayPal prompt?: y/n: ");
-		if (input.nextLine().contains("y")) {
-			System.out.print("id64: ");
-			String id = input.nextLine();
-			System.out.print("Number of keys: ");
-			double num = Double.parseDouble(input.nextLine());
-			System.out.println(question(id, num));
-			System.out.print("SourceOP? y/n: ");
-			System.out.println(leaveRep(input.nextLine().contains("y")));
-		}
-		//*/
-		// executes program through gui
-		HelperWindow window = new HelperWindow();
-		System.out.print("Classified helper?: y/n: ");
-		if (input.nextLine().contains("y")) {
-			classifiedHelper(input);
-		}
-		//roundTester(input);
-	}
+    //To do: improve GUI, make program executable?
+    public static void main (String args[]) throws FileNotFoundException  {
 
-	//used to update the prices via the console.
-	public static void updatePricesHelper(Scanner input) throws FileNotFoundException {
-		System.out.print("USD Price: ");
-		double usd = Double.parseDouble(input.nextLine());
-		System.out.print("Refined Price: ");
-		double ref = Double.parseDouble(input.nextLine());
-		updatePrices(usd, ref);
-	}
+        Scanner input = new Scanner(System.in);
+        // executes program through gui
+        HelperWindow window = new HelperWindow();
 
-	//todo: not rounding/preserving precision correctly
-	public static void classifiedHelper(Scanner input) {
-		boolean done = false;
-		while (!done) {
-			System.out.print("Price in keys: ");
-			double num = Double.parseDouble(input.nextLine());
-			int wholeNum = (int) num;
-			System.out.println(wholeNum + " keys and " + (int) (num * 100 - (wholeNum * 100)) / 100.0 * keyToRef + " Refined");
-			//*100 b/c imprecision in doubles, cast to int to truncate to only two decimal places, no rounding, need to do.
-			System.out.print("End?  Type \"y\" ");
-			done = input.nextLine().toLowerCase().contains("y");
-		}
-	}
-	
-	//pre: settings file is THE settings file created by updatePrices
-	//post: sets field variables to data saved in settings file.
-	public static void loadPrices() throws FileNotFoundException {
-		Scanner fileReader = new Scanner(new File("settings.txt"));
-		fileReader.next(); //scan past prefix/titles
-		keyPrice = fileReader.nextDouble();
-		fileReader.next();
-		keyToRef = fileReader.nextDouble();
-	}
+        //System.out.print("Classified helper?: y/n: ");
+        //if (input.nextLine().contains("y")) {
+        //    classifiedHelper(input);
+        //}
 
-	//post; saves prices passed in to a file named settings.txt
-	//ToDo update it from external pricing website
-	public static void updatePrices(double usd, double ref)  throws FileNotFoundException {
-		PrintStream output = new PrintStream(new File("settings.txt"));
-		output.println("keyPrice= " + usd);
-		output.println("keyToRef= " + ref);
-	}
-
-	//ToDo: launch various websites and search for data?  Might need to use their apis
-	public static String question(String id, double amount) {
-		double total = round(amount * keyPrice);
-		return amount + " keys will be $" + total + ", excluding fees, which are on you.  My account is US based.\n"
-		+ "Ok when ready, send $" + total + " (excluding fees) to " + email + ", YOU pay any and all fees, and put this in the notes:\n"
-		+ "I, [Real Name], steam user: " + id + " am buying " + amount + " TF2 keys, and understand that this is non-refundable and will not charge-back.";
-	}
-
-	public static String leaveRep(boolean sourceOP) {
-		if (sourceOP) {
-			return "Thanks!  Feel free to leave rep if you want!\n" + repSource +" or " + repTrust;
-		} else {
-			return "Thanks!  Feel free to leave rep if you want!\n" + repTrust + " or " + repTft;
-		}
-	}
-
-	//pre: num must be a positive double, a.k.a, the $ they have to pay
-	//post: rounds to two decimal places
-	public static double round(double num) {
-		int wholeDigits = (int) num;
-		int decimals = ((int) ((num * 1000 - wholeDigits * 1000) + 5)) / 10; 
-		// *1000/1000 because of loss of precision in storing/subtracting double
-		// cast removes digits > thousandths place, / 10 reduces to two digits
-		return wholeDigits + decimals / 100.0;
-	}
-	
-	//repeatedly test the round method
-	public static void roundTester(Scanner input) {
-		boolean done = false;
-		while (!done) {
-			System.out.print("Round this number to two decimal places: ");
-			System.out.println("Result: " + round(Double.parseDouble(input.nextLine())));
-			System.out.print("End?  Type \"y\" ");
-			done = input.nextLine().toLowerCase().contains("y");
-		}
-	}
-
+        //roundTester(input);
+    }
+    /*
+    //todo: not rounding/preserving precision correctly
+    public static void classifiedHelper(Scanner input) {
+        boolean done = false;
+        while (!done) {
+            System.out.print("Price in keys: ");
+            double num = Double.parseDouble(input.nextLine());
+            int wholeNum = (int) num;
+            System.out.println(wholeNum + " keys and " + (int) (num * 100 - (wholeNum * 100)) / 100.0 * keyToRef + " Refined");
+            //*100 b/c imprecision in doubles, cast to int to truncate to only two decimal places, no rounding, need to do.
+            System.out.print("End?  Type \"y\" ");
+            done = input.nextLine().toLowerCase().contains("y");
+        }
+    }
+    */
 }
--- HelperWindow.java
+++ HelperWindow.java
@@ -4,33 +4,53 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 /**
  * Created by Michael on 11/16/2014.
  */
 //todo: click a button to save screenshots, and have those screenshots archived with names corresponding
-    // to customers
-    // enable an entire relaunch one the user updates(so only 1 window/instance of HelperWindow running).
+// to customers
+// enable an entire relaunch one the user updates(so only 1 window/instance of HelperWindow running).
 public class HelperWindow {
+    //keyPrice and keyToRef fluctuate based on personal preference.
+    //"keys" and "Refined" or "Ref" are in-game currencies.
+    public double keyPrice;
+    public double keyToRef;
+    public double budToKey;
+    public double totalSum; // total sum of transaction
+
+    //Personal user information.
+    private static String repTft = "";
+    private static String repSource = "";
+    private static String repTrust = "";
+    public static String email = "";
+
     //Constants for the window/gui
     public static final int WINDOW_WIDTH = 800;
     public static final int WINDOW_HEIGHT = 400;
 
     //References
     private JPanel panel;
-    private JLabel prompt;
-    private JLabel keyPrice;
-    private JLabel keyToRef;
+    private JLabel keyPriceLabel;
+    private JLabel keyToRefLabel;
+    private JLabel budToKeyLabel;
     private JLabel idLabel;
-    private JLabel keyLabel;
+    private JLabel amountLabel;
+    private JTextField textFieldBudToKey;
     private JTextField textFieldKeysUSD;
-    private JTextField textFieldKeysRef;
+    private JTextField textFieldKeysToRef;
     private JTextField textFieldID;
-    private JTextField textFieldKeys;
+    private JTextField textFieldBudAmount;
+    private JTextField textFieldKeyAmount;
+    private JTextField textFieldRefAmount;
+    private JButton buttonPrompt;
     private JButton buttonUpdate;
-    private JButton buttonPrice;
+    private JButton buttonCalculate;
+    private JButton buttonAffadavit;
     private JButton buttonRepSource;
     private JButton buttonRep;
 
@@ -55,24 +75,29 @@ public class HelperWindow {
     }
 
     public void buildPanel() throws FileNotFoundException {
-        String initPrompt = "Ok, where did you see my listing, how many keys do you want, "
-                + "do you have any rep threads, and from what e-mail will you be sending from?";
-        prompt = new JLabel(initPrompt);
-        copyToClipboard(initPrompt);
-        HelperMain.loadPrices();
-        keyPrice = new JLabel("Key Price in USD: " + HelperMain.keyPrice);
-        keyToRef = new JLabel("Key Price in Refined: " + HelperMain.keyToRef);
-        textFieldKeysUSD = new JTextField(3);
-        textFieldKeysRef = new JTextField(4);
+        loadPrices();
+        buttonPrompt = new JButton("Prompt");
+        buttonPrompt.addActionListener(new HelperWindowListener());
+        budToKeyLabel = new JLabel("Bud price in keys: " + budToKey);
+        keyPriceLabel = new JLabel("Key Price in USD: " + keyPrice);
+        keyToRefLabel = new JLabel("Key Price in Refined: " + keyToRef);
+        textFieldKeysUSD = new JTextField(4);
+        textFieldKeysToRef = new JTextField(4);
+        textFieldBudToKey = new JTextField(4);
         buttonUpdate = new JButton("Update Prices");
         buttonUpdate.addActionListener(new HelperWindowListener());
 
         idLabel = new JLabel("Enter steam 64id: ");
         textFieldID = new JTextField(20);
-        keyLabel = new JLabel("Number of keys:");
-        textFieldKeys = new JTextField(20);
-        buttonPrice = new JButton("Calculate:");
-        buttonPrice.addActionListener(new HelperWindowListener());
+        amountLabel = new JLabel("Number of buds/keys/ref:");
+        textFieldBudAmount = new JTextField(4);
+        textFieldKeyAmount = new JTextField(4);
+        textFieldRefAmount = new JTextField(4);
+        buttonCalculate = new JButton("Calculate:");
+        buttonCalculate.addActionListener(new HelperWindowListener());
+
+        buttonAffadavit = new JButton("Affadavit");
+        buttonAffadavit.addActionListener(new HelperWindowListener());
 
         buttonRepSource = new JButton("SourceOP Rep");
         buttonRepSource.addActionListener(new HelperWindowListener());
@@ -80,52 +105,177 @@ public class HelperWindow {
         buttonRep.addActionListener(new HelperWindowListener());
 
         panel = new JPanel();
-        panel.add(prompt);
-        panel.add(keyPrice);
-        panel.add(keyToRef);
+        panel.add(buttonPrompt);
+        panel.add(budToKeyLabel);
+        panel.add(keyPriceLabel);
+        panel.add(keyToRefLabel);
+        panel.add(textFieldBudToKey);
         panel.add(textFieldKeysUSD);
-        panel.add(textFieldKeysRef);
+        panel.add(textFieldKeysToRef);
         panel.add(buttonUpdate);
         panel.add(idLabel);
         panel.add(textFieldID);
-        panel.add(keyLabel);
-        panel.add(textFieldKeys);
-        panel.add(buttonPrice);
+        panel.add(amountLabel);
+        panel.add(textFieldBudAmount);
+        panel.add(textFieldKeyAmount);
+        panel.add(textFieldRefAmount);
+        panel.add(buttonCalculate);
+        panel.add(buttonAffadavit);
         panel.add(buttonRepSource);
         panel.add(buttonRep);
     }
 
-    // needs to refresh table with loaded prices
+    // needs to refresh table with loaded prices, and fix the extraneous notification from updating price
     private class HelperWindowListener implements ActionListener {
+
+        //parse the double in the textwindow if it exists, if not, set it to 0 automatically
+        public double getTextFieldDouble(JTextField input) {
+            if (!input.getText().isEmpty()) {
+                return Double.parseDouble(input.getText());
+            } else {
+                return 0;
+            }
+        }
+
         public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == buttonPrice) {
-                String id = textFieldID.getText();
-                double keyAmount = Double.parseDouble(textFieldKeys.getText());
-                String output = HelperMain.question(id, keyAmount);
-                copyToClipboard(output);
-                JOptionPane.showMessageDialog(null, output);
+            String output;
+            double budAmount = getTextFieldDouble(textFieldBudAmount);
+            double keyAmount = getTextFieldDouble(textFieldKeyAmount);
+            double refAmount = getTextFieldDouble(textFieldRefAmount);
+            if (e.getSource() == buttonCalculate) {
+                output = calculate(budAmount, keyAmount, refAmount) + " will be $" + totalSum
+                        + " excluding fees, which are on you.  My account is US based.";;
             } else if (e.getSource() == buttonUpdate) {
-                double usd = Double.parseDouble(textFieldKeysUSD.getText());
-                double ref = Double.parseDouble(textFieldKeysRef.getText());
-                String output = "updated";
+                // if user inputs 0 or nothing, it doesn't update the price
+                // assumed that none of the prices will be $0
+                double bud = getTextFieldDouble(textFieldBudToKey);
+                if (bud == 0) {
+                    bud = budToKey;
+                }
+                double usd = getTextFieldDouble(textFieldKeysUSD);
+                if (usd == 0) {
+                    usd = keyPrice;
+                }
+                double ref = getTextFieldDouble(textFieldKeysToRef);
+                if (ref == 0) {
+                    ref = keyToRef;
+                }
+                output = "updated";
                 try {
-                    HelperMain.updatePrices(usd, ref);
-                    HelperMain.loadPrices();
-                    JOptionPane.showMessageDialog(null, output);
+                    updatePrices(bud, usd, ref);
+                    loadPrices();
                     new HelperWindow();
                 } catch (FileNotFoundException e1) {
                     e1.printStackTrace();
                 }
-            } else if (e.getSource() == buttonRepSource) {
-                String output = HelperMain.leaveRep(true);
-                copyToClipboard(output);
-                JOptionPane.showMessageDialog(null, output);
-            } else {//if (e.getSource() == buttonRep) {
-                String output = HelperMain.leaveRep(false);
-                copyToClipboard(output);
-                JOptionPane.showMessageDialog(null, output);
+            } else if (e.getSource() == buttonPrompt) {
+                output = "Ok, where did you see my listing, how many keys do you want, "
+                        + "do you have any rep threads, and from what e-mail will you be sending from?";
+            } else if (e.getSource() == buttonAffadavit) {
+                output = affadavit(textFieldID.getText(), budAmount, keyAmount, refAmount);
+            } else {
+                output = leaveRep(e.getSource() == buttonRepSource);
             }
+            copyToClipboard(output);
+            JOptionPane.showMessageDialog(null, output);
         }
     }
+
+    //pre: settings file is THE settings file created by updatePrices
+    //post: sets field variables to data saved in settings file.
+    public void loadPrices() throws FileNotFoundException {
+        File settings = new File("settings.txt");
+        if (settings.exists()) {
+            Scanner fileReader = new Scanner(settings);
+            fileReader.next(); //scan past prefix/titles
+            budToKey = fileReader.nextDouble();
+            fileReader.next();
+            keyPrice = fileReader.nextDouble();
+            fileReader.next();
+            keyToRef = fileReader.nextDouble();
+
+        } else {
+            updatePrices(0, 0, 0); //need to make this better
+        }
+    }
+
+    //post; saves prices passed in to a file named settings.txt
+    //ToDo update it from external pricing website
+    public void updatePrices(double bud, double usd, double ref)  throws FileNotFoundException {
+        PrintStream output = new PrintStream(new File("settings.txt"));
+        output.println("budToKey= " + bud);
+        output.println("keyPrice= " + usd);
+        output.println("keyToRef= " + ref);
+    }
+
+    //ToDo: launch various websites and search for data?  Might need to use their apis
+    // calculates total price and returns the string of the amount of each item being sold
+    public String calculate(double budAmount, double keyAmount, double refAmount) {
+        totalSum = round((budAmount * budToKey + keyAmount + (refAmount / keyToRef)) * keyPrice);
+        int commaCount = 0;
+        String bud = "";
+        String key = "";
+        String ref = "";
+        if (budAmount != 0) {
+            bud = budAmount + " TF2 earbuds"; //earbuds is the same for both singular/plural
+            commaCount++;
+        }
+        if (keyAmount != 0) {
+            key = keyAmount + " TF2 key";
+            if (keyAmount > 1) {
+                key = key + "s";
+            }
+            commaCount++;
+        }
+        if (refAmount != 0) {
+            ref = refAmount + " TF2 refined metal";
+            commaCount++;
+        }
+        String result = bud + key + ref;
+        if (commaCount == 2) {
+            if (!bud.isEmpty()) {
+                result = bud + " and " + key + ref;
+            }  else {
+                result = key + " and " + ref;
+            }
+        } else if (commaCount == 3) { // commaCount == 3
+            result = bud + ", " + key + ", and " + ref;
+        }
+        return result;
+    }
+
+    public String affadavit(String id, double budAmount, double keyAmount, double refAmount) {
+        String result = calculate (budAmount, keyAmount, refAmount);
+        return "Ok when ready, send $" + totalSum + " (excluding fees) to " + email + ", YOU pay any and all fees, and put this in the notes:\n"
+                + "I, [Real Name], steam user: " + id + " am buying " + result + ", and understand that this is non-refundable and will not charge-back.";
+    }
+
+    public String leaveRep(boolean sourceOP) {
+        if (sourceOP) {
+            return "Thanks!  Feel free to leave rep if you want!\n" + repSource +" or " + repTrust;
+        } else {
+            return "Thanks!  Feel free to leave rep if you want!\n" + repTrust + " or " + repTft;
+        }
+    }
+
+    //pre: num must be a positive double, a.k.a, the $ they have to pay
+    //post: rounds to two decimal places
+    public double round(double num) {
+        double addFive = (num * 1000) + 5;
+        int rounded = (int) (addFive / 10);
+        return rounded / 100.0;
+    }
+
+    //repeatedly test the round method
+    public void roundTester(Scanner input) {
+        boolean done = false;
+        while (!done) {
+            System.out.print("Round this number to two decimal places: ");
+            System.out.println("Result: " + round(Double.parseDouble(input.nextLine())));
+            System.out.print("End?  Type \"y\" ");
+            done = input.nextLine().toLowerCase().contains("y");
+        }
+    }
+
 
 }
